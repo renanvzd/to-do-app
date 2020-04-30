bea10652c7a880fbaacf55e68acb332974774f4d
Renan <renanvz@outlook.com>
2020-04-30 19:34:34 -0300

    CRUD - Update

--- server.js
+++ server.js
@@ -4,12 +4,15 @@ let mongodb = require('mongodb')
 let app = express()
 let db
 
+app.use(express.static('public'))
+
 let connectionString = 'mongodb+srv://todoAppUser:test@cluster0-7784w.gcp.mongodb.net/TodoApp?retryWrites=true&w=majority'
 mongodb.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true}, function(err, client) {
     db = client.db()
     app.listen(3000)
 })
 
+app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
 app.get('/', function(req, res) {
@@ -34,7 +37,6 @@ app.get('/', function(req, res) {
               </div>
             </form>
           </div>
-          
           <ul class="list-group pb-5">
             ${items.map(function(item) {
               return `<li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
@@ -46,18 +48,22 @@ app.get('/', function(req, res) {
               </li>`
             }).join('')}
           </ul>
-          
         </div>
-        
+
+        <script src="https://unpkg.com/axios/dist/axios.min.js"></script> 
+        <script src="/browser.js"></script>
       </body>
       </html>`)
     })
-   
 })
 
 app.post('/create-item', function(req, res){
     db.collection('items').insertOne({text: req.body.item}, function(){
         res.redirect('/')
     })
-    
+})
+
+app.post('/update-item', function(req, res){
+    console.log(req.body.text)
+    res.send("Success")
 })
