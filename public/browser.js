849c49dc1600458887fda0de7b65b927ecc90bfd
Renan <renanvz@outlook.com>
2020-05-01 17:27:14 -0300

    CRUD - Update (without page reload)

--- public/browser.js
+++ public/browser.js
@@ -1,3 +1,27 @@
+function itemTemplate(item) {
+    return `<li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
+    <span class="item-text">${item.text}</span>
+    <div>
+      <button data-id="${item._id}" class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
+      <button data-id="${item._id}" class="delete-me btn btn-danger btn-sm">Delete</button>
+    </div>
+    </li>`
+}
+
+//CREATE FEATURE
+let createField = document.getElementById("create-field")          //here is where we actually selected the html input field to reusable the variable. This variable can be used always we need access and word with this element (Date on field "Add new item") 
+document.getElementById("create-form").addEventListener("submit", function(e) {
+    e.preventDefault()
+    axios.post('/create-item', {text: createField.value}).then(function (response){
+        // Create the HTML for a new item
+        document.getElementById("item-list").insertAdjacentHTML("beforeend", itemTemplate(response.data))
+        createField.value = ""
+        createField.focus()
+    }).catch(function(){
+        console.log("Please try again later.")
+    })
+})
+
 document.addEventListener("click", function(e) {
     //DELETE FEATURE
     if (e.target.classList.contains("delete-me")) {
@@ -10,7 +34,7 @@ document.addEventListener("click", function(e) {
             }
         }
 
-    //UPDATE
+    //UPDATE FEATURE
     if (e.target.classList.contains("edit-me")) {
         let userInput = prompt("Enter you desired new text", e.target.parentElement.parentElement.querySelector(".item-text").innerHTML)
         if (userInput) {
