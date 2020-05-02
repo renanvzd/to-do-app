cdb1a5535d718fd05ebacf5457e874ac1abe4188
renanvzd <renanvz@outlook.com>
2020-05-02 19:06:03 -0300

    Connect to Heroku

--- server.js
+++ server.js
@@ -5,12 +5,17 @@ let sanitizeHTML = require('sanitize-html')
 let app = express()
 let db
 
+let port = process.env.PORT
+if (port == null || port == "") {
+  port = 3000
+}
+
 app.use(express.static('public'))
 
 let connectionString = 'mongodb+srv://todoAppUser:test@cluster0-7784w.gcp.mongodb.net/TodoApp?retryWrites=true&w=majority'
 mongodb.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true }, function (err, client) {
   db = client.db()
-  app.listen(3000)
+  app.listen(port)
 })
 
 app.use(express.json())
